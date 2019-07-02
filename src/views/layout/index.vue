bfb8de466d79c830e0fbef5543198c3f2ffded65
wangnewbie <wangnewbie@163.com>
2019-07-02 15:19:46 +0800

    禁止路由缓存

--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -9,7 +9,7 @@
           <AddHeader></AddHeader>
         </el-header>
         <el-main>
-          <router-view></router-view>
+          <router-view :key="$route.path"></router-view>
         </el-main>
       </el-container>
     </el-container>
